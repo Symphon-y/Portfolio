c9cd7e53ff9660e56eaaf96cd74ac05f7b7b4b12
Travis Redden <travisredden@outlook.com>
2022-11-07 00:08:03 -0600

    fixed laptop light

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -30,7 +30,7 @@ const Experience = () => {
         <PresentationControls
           global
           rotation={[0.13, 0.5, 0]}
-          polar={[-0.4, 0.2]}
+          polar={[-0.25, 0.2]}
           azimuth={[-1, 0.75]}
           config={{ mass: 2, tension: 400 }}
           snap={{ mass: 4, tension: 400 }}
@@ -44,7 +44,7 @@ const Experience = () => {
               intensity={65}
               color={'#57cbff'}
               rotation={[-0.1, Math.PI, 0]}
-              position={[0, 0.55, -1.15]}
+              position={[0, 1.75, -1.15]}
             />
             {/* <ClickToZoom> */}
             <mesh position-y={-1.2} scale={1.25}>
